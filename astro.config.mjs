6f03ba6992713c9b7f4e3cdd3ed588921a337653
krzysztofpiesio89 <98741240+krzysztofpiesio89@users.noreply.github.com>
2026-07-21 20:32:23 +0200

    ci: add GitHub Actions workflow and config for GitHub Pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,13 +3,11 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://krzysztofpiesio89.github.io',
+  base: '/bak-landing',
   output: 'static',
   compressHTML: true,
   build: {
     inlineStylesheets: 'auto',
-  },
-  image: {
-    // Use sharp for image optimization
-    service: { entrypoint: 'astro/assets/services/sharp' },
   },
 });
