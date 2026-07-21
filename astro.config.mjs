ad14a2ef6a834ffefb430159753a779c3e83c389
krzysztofpiesio89 <98741240+krzysztofpiesio89@users.noreply.github.com>
2026-07-22 00:37:12 +0200

    fix: conditional base path in astro config for seamless dev server & gh pages build

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://krzysztofpiesio89.github.io',
-  base: '/bak-landing',
+  base: process.env.NODE_ENV === 'production' ? '/bak-landing' : '/',
   output: 'static',
   compressHTML: true,
   build: {
